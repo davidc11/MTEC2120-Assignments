0086e249b5162781416291a969ce71d845db4b0e
David Calixto <davidcalixto9@gmail.com>
2019-10-06 23:16:14 -0400

    Pushing Project Onto Repository

---
+++ Project-1/Assets/Destroy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Cube")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
+
---
+++ Project-1/Assets/UserInteraction.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInteraction : MonoBehaviour
+{
+    public float speed = 1;
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0, 10 * Time.deltaTime * speed, 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(0, -10 * Time.deltaTime * speed, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-10 * Time.deltaTime * speed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(10 * Time.deltaTime * speed, 0, 0);
+        }
+    }
+}
+
